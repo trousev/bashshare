cf43375a39509e7e92fb9b73124b104a539e6d8c
Alexander Trousevich <trousev@yandex.ru>
2014-06-12 02:26:48 +0400

    added gpg

---
+++ src/test/test_io_gpg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+export BS_STORAGE=gpg
+export BS_GPG_EMAIL=trousev@yandex.ru
+
+export BS_NAME=test
+export BS_VARS="var1 var2 var3 username host"
+
+export var1=foo
+export var2=bar
+export username=user
+export host=example.com
+
+source ../$BS_STORAGE/write.sh
+export BS_NAME=test2
+export username=test
+source ../$BS_STORAGE/write.sh
+
+export BS_NAME=test
+source ../$BS_STORAGE/read.sh
+[ $username == "user" ] || exit 1
+
+
+export BS_NAME=test2
+source ../$BS_STORAGE/read.sh
+[ $username == "test" ] || exit 1
+
