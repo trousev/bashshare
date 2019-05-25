d00f7efe8cec2c78522232be08b7c6b186bcc7c4
Alexander Trousevich <trousev@yandex.ru>
2019-05-25 20:55:00 +0300

    iup

--- src/smb/mount.sh
+++ src/smb/mount.sh
@@ -4,5 +4,6 @@ mkdir -p $BS_OUTPUT
 mountpoint $BS_OUTPUT 1> /dev/null && echo "Already mounted"
 mountpoint $BS_OUTPUT 1> /dev/null && exit 1
 
-$BS_SU mount //$BS_HOST/$BS_FOLDER $BS_OUTPUT -t cifs -o username=$BS_DOMAIN/$BS_USER,password=$BS_PASSWORD,uid=$(id -u),gid=$(id -g),user,codepage=cp1251,iocharset=utf8
+# OLD: $BS_SU mount //$BS_HOST/$BS_FOLDER $BS_OUTPUT -t cifs -o username=$BS_DOMAIN/$BS_USER,password=$BS_PASSWORD,uid=$(id -u),gid=$(id -g),user,codepage=cp1251,iocharset=utf8
+$BS_SU mount //$BS_HOST/$BS_FOLDER $BS_OUTPUT -t cifs -o username=$BS_USER,password=$BS_PASSWORD,uid=$(id -u),gid=$(id -g),user,iocharset=utf8
 
