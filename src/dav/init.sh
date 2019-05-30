c137f517e527c1a89a6b1cd4be79773996566a94
trousevarchive <trousevarchive@penguin>
2019-05-30 13:06:29 +0300

    up

--- src/dav/init.sh
+++ src/dav/init.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-BS_VARS="BS_SCHEMA BS_HOST BS_USER BS_PASSWORD BS_FOLDER"
+BS_VARS="BS_HOST BS_USER BS_PASSWORD"
 
-export BS_SCHEMA_CAPTION="Schema -- http or https"
-export BS_SCHEMA_DEFAULT="https"
-export BS_HOST_CAPTION="Host name"
-export BS_USER_CAPTION="User name"
+export BS_HOST_CAPTION="Server URL"
+export BS_USER_CAPTION="Login"
 export BS_PASSWORD_CAPTION="Password"
 export BS_PASSWORD_TYPE="password"
-export BS_FOLDER_CAPTION="Remote folder"
 
--- src/dav/mount.sh
+++ src/dav/mount.sh
@@ -4,5 +4,6 @@ mkdir -p $BS_OUTPUT
 mountpoint $BS_OUTPUT 1> /dev/null && echo "Already mounted"
 mountpoint $BS_OUTPUT 1> /dev/null && exit 1
 
-$BS_SU echo $BS_PASSWORD | $BS_SU mount $BS_SCHEMA://$BS_HOST/$BS_FOLDER $BS_OUTPUT -t davfs -o uid=$(id -u),gid=$(id -g),user,username=$BS_USER
+# $BS_SU echo $BS_PASSWORD | $BS_SU mount $BS_SCHEMA://$BS_HOST/$BS_FOLDER $BS_OUTPUT -t davfs -o uid=$(id -u),gid=$(id -g),user,username=$BS_USER
+echo $BS_PASSWORD | $BS_SU mount $BS_HOST $BS_OUTPUT -t davfs -o uid=$(id -u),gid=$(id -g),user,username=$BS_USER
 
